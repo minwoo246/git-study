cee294d1fb7bd3c22e60e3ba554a90601e39334a
minwoo246 <smw85563@gmail.com>
2025-04-30 14:46:04 +0900

    py.py

---
+++ rinux/GPO.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+LED = 11,13,15
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+try:
+	while 1:
+		GPIO.output(LED, GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(LED, GPIO.LOW)
+		time.sleep(0.5)
+except KeyboardInterrupt:
+	pass
+finally:
+	GPIO.cleanup()
---
+++ rinux/py.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+LED = 11
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+try:
+	while 1:
+		swh = input()
+		if swh == '1':
+	     		GPIO.output(LED,GPIO.HIGH)
+		else:
+			GPIO.output(LED,GPIO.LOW)
+except KeyboardInterrupt:
+	pass
+finally:
+	GPIO.cleanup()
